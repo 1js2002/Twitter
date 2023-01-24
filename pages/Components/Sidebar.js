3652eac1d4df4859d244d58562c0947b21fdac4a
1js2002 <dgoud99120@gmail.com>
2023-01-24 12:57:50 +0530

    create feed component and its header

--- pages/Components/Sidebar.js
+++ pages/Components/Sidebar.js
@@ -4,7 +4,7 @@ import {BellIcon, BookmarkIcon, ClipboardIcon, DotsCircleHorizontalIcon, DotsHor
 
 export default function Sidebar() {
   return (
-    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
         <div className="hoverEffect p-0 hover:bg-blue-100 ">
         {/* twitter logo */}
         <Image
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Sidebar from './Components/Sidebar'
-
+import Feed from './Components/Feed'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,11 +14,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen max-w-5xl mx-auto ">
+      <main className="flex min-h-screen mx-auto">
 
         {/* sidebar */}
         <Sidebar/>
         {/* feed */}
+        <Feed/>
 
         {/* widgets */}
 
