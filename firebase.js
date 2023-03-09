3c46c67373d2bd58d0417e666a428aa44c2472bc
1js2002 <dgoud99120@gmail.com>
2023-03-09 23:58:25 +0530

    install nextauth and initialize firebase 2

--- firebase.js
+++ firebase.js
@@ -1,22 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
-import {getFireStore} from "firebase/getFireStore"
-import {getStorage} from "firebase/storage"
+import { initializeApp, getApp, getApps, } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+import { getStorage } from "firebase/syorage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "twitter-v1-324c5.firebaseapp.com",
-  projectId: "twitter-v1-324c5",
-  storageBucket: "twitter-v1-324c5.appspot.com",
-  messagingSenderId: "146345423625",
-  appId: "1:146345423625:web:2f3d9b3908bed70652d9d8"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "twitterfinal-8e753.firebaseapp.com",
+  projectId: "twitterfinal-8e753",
+  storageBucket: "twitterfinal-8e753.appspot.com",
+  messagingSenderId: "874701516658",
+  appId: "1:874701516658:web:a982cceb21d3ee072ef7eb",
+  measurementId: "G-FGFT8NK2R6"
 };
 
 // Initialize Firebase
-const app = !getApp.length() ?  initializeApp(firebaseConfig) : getApp();
-const db = getFireStore();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
 const storage = getStorage();
-export {app , db, storage}
+export { app, db, storage };
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,29 +1,15 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId: process.env.GOOGLE_CLIENT,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
-  pages: {
-    signIn: "/auth/signin",
-  },
-
-  callbacks: {
-    async session({ session, token }) {
-      session.user.username = session.user.name
-        .split(" ")
-        .join("")
-        .toLocaleLowerCase();
-      session.user.uid = token.sub;
-      return session;
-    },
-  },
-};
+}
 
 export default NextAuth(authOptions)
